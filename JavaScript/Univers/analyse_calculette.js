a0bca3d81228ac39c80cbe2a3d66a836d26815f8
Joanny-Magat <joanny.magat@etu.umontpellier.fr>
2026-04-27 11:57:33 +0200

    Description du js analyse_calculette

---
+++ JavaScript/Univers/analyse_calculette.js
@@ -0,0 +1,19 @@
+/*
+Ce fichier est le javascript des deux pages d'analyse de la calculette LCDM et DE.
+Il permet de comparer les calculs effectués par les javascript du site avec ceux d'un autre logiciel afin de confirmer la validité des calculs du site.
+*/
+
+function fonction1() { //à renommer
+    let texte = o_recupereJson();
+    //à faire
+}
+
+function fonction2() { //à renommer
+    let texte = o_recupereJson();
+    //à faire
+}
+
+function fonction3() { //à renommer
+    let texte = o_recupereJson();
+    //à faire
+}
